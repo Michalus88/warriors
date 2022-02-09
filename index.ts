386d9241d4e56c4aa0f9fb4ec024097e4df6721b
Michal Molenda <michalus88@gmail.com>
2022-02-09 22:31:29 +0100

    initial commit with basic express template

---
+++ index.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import * as express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    // helpers: handlebarsHelpers,
+  })
+);
+app.set("view engine", "hbs");
+
+// app.use(errorHandler);
+
+app.listen(3000, () => console.log("Server listening on port 3000"));
