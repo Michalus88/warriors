fa03b9bf049bc1d97893c9b1e8e2a2640e8b5d60
Michal Molenda <michalus88@gmail.com>
2022-02-15 01:45:08 +0100

    added style for nav, and register view

--- routers/register-routers.ts
+++ routers/register-routers.ts
@@ -5,13 +5,18 @@ import { Warrior } from "../records/warrior.record";
 export default () => {
   const registerRouter = Router();
 
-  registerRouter.post("/", async (req: Request, res: Response) => {
-    const warriorRegister = req.body as WarriorData;
+  registerRouter
+    .get("/", (req: Request, res: Response) => {
+      res.render("register");
+    })
 
-    const newWarrior = new Warrior(warriorRegister);
-    const warr = await newWarrior.insert();
-    res.send(warr);
-  });
+    .post("/", async (req: Request, res: Response) => {
+      const warriorRegister = req.body as WarriorData;
+
+      const newWarrior = new Warrior(warriorRegister);
+      const warr = await newWarrior.insert();
+      res.render("home");
+    });
 
   return registerRouter;
 };
