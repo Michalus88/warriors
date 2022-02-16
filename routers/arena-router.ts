ed6c87766bc51192d66d59f3d806b96bc9b9b841
Michal Molenda <michalus88@gmail.com>
2022-02-16 17:35:20 +0100

    implemented arenaController in arenaRouter

--- routers/arena-router.ts
+++ routers/arena-router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import arenaController from "../controllers/arena-controller";
 
 export default () => {
   const arenaRouter = Router();
+
+  arenaRouter
+    .get("/", arenaController.pageRender)
+    .post("/", arenaController.fight);
 
   return arenaRouter;
 };
