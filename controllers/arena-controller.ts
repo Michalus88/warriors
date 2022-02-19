107db41c8b3189ff707d87b33e526fa4a0899dcc
Michal Molenda <michalus88@gmail.com>
2022-02-20 00:04:52 +0100

    created methods to initialize data and fight algorithms

--- controllers/arena-controller.ts
+++ controllers/arena-controller.ts
@@ -32,15 +32,71 @@ export class Arena {
     res.render("arena/fight");
   };
 
-  setWarriors = async (firstFighter, secondFighter) => {
-    this.warrior1 = await Warrior.findOneByName(firstFighter);
-    this.warrior2 = await Warrior.findOneByName(secondFighter);
+  private setWarriors = async (
+    firstFighterName,
+    secondFighterName
+  ): Promise<void> => {
+    this.whoIsAttacking = this.attackerDraw();
+    const warrior1 = await Warrior.findOneByName(firstFighterName);
+    const warrior2 = await Warrior.findOneByName(secondFighterName);
     if (this.warrior1 === null || this.warrior2 === null) {
       throw new Error("Nie posiadamy zawodnika o podanej nazwie");
     }
+    this.warrior1 = this.setWarrior(warrior1);
+    this.warrior2 = this.setWarrior(warrior2);
   };
 
-  validate = async (firstFighter: FighterName, secondFighter: FighterName) => {
+  private attackerDraw = (): AtackerNumber =>
+    Math.floor(Math.random() * (2 - 1 + 1) + 1) as AtackerNumber;
+
+  private setWarrior = (warrior: Warrior): Fighter => ({
+    record: warrior,
+    hp: warrior.resilience * 10,
+    dp: warrior.defense,
+    shield:
+      warrior.strength < warrior.defense + warrior.agility
+        ? warrior.defense
+        : null,
+  });
+
+  private fightResult = (): void => {
+    this.logs = [];
+    let winner: Fighter;
+    let attacker = this.whoIsAttacking === 1 ? this.warrior1 : this.warrior2;
+    let defender = this.whoIsAttacking === 1 ? this.warrior2 : this.warrior1;
+
+    do {
+      if (attacker.shield && defender.shield > 0) {
+        defender.dp -= attacker.record.strength;
+        if (defender.dp < 0) {
+          defender.hp -= -defender.dp;
+          this.logs.push(
+            `Obrona ${defender.record.name} została przełamana i ${attacker.record.name} zadał obrażenia w wysokości 
+            ${attacker.record.strength}`
+          );
+        } else {
+          this.logs.push(`Obrona przez ${defender.record.name} powiodła się.`);
+        }
+      } else {
+        defender.hp -= attacker.record.strength;
+        this.logs.push(
+          `${attacker.record.name} zadał obrażenia ${defender.record.name} w wysokości ${attacker.record.strength}`
+        );
+      }
+      if (defender.hp >= 0) {
+        winner = attacker;
+      }
+      [attacker, defender] = [defender, attacker];
+    } while (attacker.hp >= 0);
+    this.logs.push(`Zwycięża ${winner.record.name} !!!`);
+
+    winner.record.winningsInc();
+  };
+
+  private validate = (
+    firstFighterName: FighterName,
+    secondFighterName: FighterName
+  ): void => {
     if (
       firstFighter === (undefined || "") ||
       secondFighter === (undefined || "")
